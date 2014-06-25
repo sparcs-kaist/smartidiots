f67662a2db409192cf5536ec3645f20d11b53eca
Changje Jeong <changjej@tgrape.com>
2014-06-25 15:21:56 +0900

    add member urls.py

--- smartidiots/member/views.py
+++ smartidiots/member/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+@require_http_methods(["GET"])
+def listView(request):
+    return HttpResponse("Member ListView")
