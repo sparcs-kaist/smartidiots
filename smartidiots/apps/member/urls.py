90eb39d01d6c39d005aad302379410befa44062a
Changje Jeong <changjej@tgrape.com>
2014-06-26 10:32:57 +0900

    member detail view func

--- smartidiots/apps/member/urls.py
+++ smartidiots/apps/member/urls.py
@@ -1,9 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'smartidiots.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', 'apps.member.views.listView'),
+    url(r'^$', 'apps.member.views.list'),
+    url(r'^(?P<username>\w+)$', 'apps.member.views.detail'),
 )
--- smartidiots/apps/member/views.py
+++ smartidiots/apps/member/views.py
@@ -1,10 +1,22 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from models import Member
 from django.views.decorators.http import require_http_methods
+from django.core.exceptions import ObjectDoesNotExist
+from django.forms.models import model_to_dict
 
 
 @require_http_methods(["GET"])
-def listView(request):
+def list(request):
     members = Member.objects.all()
 
     return render(request,'member/list.html',{"members":members})
+
+@require_http_methods(["GET"])
+def detail(request, username):
+    try:
+        member = Member.objects.get(sparcsid=username)
+    except ObjectDoesNotExist:
+        return HttpResponse(content="Member not found", status=404)
+    data = model_to_dict(member)
+    return render(request,'member/detail.html',{"member":data})
